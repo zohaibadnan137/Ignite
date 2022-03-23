1f3e311b741fc5950554628e6a63bd313b03603c
zohaibadnan137 <zohaibadnan137@gmail.com>
2022-03-24 01:47:26 +0500

    Added splash screen

--- src/main/java/runtimeterror/userinterface/Main.java
+++ src/main/java/runtimeterror/userinterface/Main.java
@@ -2,19 +2,20 @@ package runtimeterror.userinterface;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class Main extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage primaryStage) throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("SplashScreen.fxml")));
+        primaryStage.setTitle("Ignite");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
---
+++ src/main/java/runtimeterror/userinterface/SplashScreenEventHandler.java
@@ -0,0 +1,35 @@
+package runtimeterror.userinterface;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
+
+public class SplashScreenEventHandler {
+
+    @FXML
+    private void loadCustomerLoginScreen(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("hello-view.fxml")));
+        Stage primaryStage = new Stage();
+        primaryStage.setTitle("Customer Login");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+        ((Node) (event.getSource())).getScene().getWindow().hide();
+    }
+
+    @FXML
+    private void loadEmployeeLoginScreen(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("EmployeeLogin.fxml")));
+        Stage primaryStage = new Stage();
+        primaryStage.setTitle("Employee Login");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+        ((Node) (event.getSource())).getScene().getWindow().hide();
+    }
+}
