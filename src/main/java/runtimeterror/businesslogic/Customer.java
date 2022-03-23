429bffb6ab976e158c226e334524f7fd9a938f73
zohaibadnan137 <zohaibadnan137@gmail.com>
2022-03-23 20:46:11 +0500

    Added customer class and ID dispenser utility

---
+++ src/main/java/runtimeterror/businesslogic/Customer.java
@@ -0,0 +1,38 @@
+package runtimeterror.businesslogic;
+
+import runtimeterror.utlities.IdDispenser;
+
+import java.io.FileNotFoundException;
+
+public class Customer {
+
+    private final int customer_id;
+    private final String customer_name;
+    private final int customer_account_number;
+    private final String username;
+    private final String password;
+
+    public Customer(String customer_name, int customer_account_number, String username, String password) throws FileNotFoundException {
+        this.customer_id = IdDispenser.getID();
+        this.customer_name = customer_name;
+        this.customer_account_number = customer_account_number;
+        this.username = username;
+        this.password = password;
+    }
+
+    public int getCustomerId() {
+        return customer_id;
+    }
+
+    public String getCustomerName() {
+        return customer_name;
+    }
+
+    public int getCustomerAccountNumber() {
+        return customer_account_number;
+    }
+
+    public boolean authenticateCustomer(String username, String password) {
+        return this.username.equals(username) && this.password.equals(password);
+    }
+}
---
+++ src/main/java/runtimeterror/utlities/IdDispenser.java
@@ -0,0 +1,18 @@
+package runtimeterror.utlities;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class IdDispenser {
+
+    public static int getID() throws FileNotFoundException {
+        Scanner input = new Scanner(new File(System.getProperty("user.dir") + "/src/utilities/id.txt"));
+        int id = input.nextInt() + 1;
+        PrintStream output = new PrintStream(System.getProperty("user.dir") + "/src/utilities/id.txt");
+        output.print(id);
+        output.close();
+        return id;
+    }
+}
