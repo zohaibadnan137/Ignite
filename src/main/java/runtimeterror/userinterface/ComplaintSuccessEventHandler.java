14af1e8db561792a49a4c1a4eb17ed980db82527
MuhammadMoiz2050 <moiz2050@gmail.com>
2022-04-17 19:49:58 +0500

    Added complaint and complaint success UI files.

---
+++ src/main/java/runtimeterror/userinterface/ComplaintSuccessEventHandler.java
@@ -0,0 +1,27 @@
+package runtimeterror.userinterface;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
+
+public class ComplaintSuccessEventHandler {
+
+    @FXML
+    private void redirect(ActionEvent event) throws IOException {
+        //Add the main menu fxml in the code-line below
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("MainInterface.fxml")));
+        Stage primaryStage = new Stage();
+        primaryStage.setTitle("Customer Login");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.setResizable(false);
+        primaryStage.show();
+        ((Node) (event.getSource())).getScene().getWindow().hide();
+    }
+}
